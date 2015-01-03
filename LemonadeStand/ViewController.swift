99efb8734f8e54e73ccef20307a95d74d34bc1c4
James Dykstra <jgd@xpressmo.com>
2015-01-03 11:10:23 -0800

    Connected all Storyboard labels and buttons to ViewController

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cashTotalLabel: UILabel!
+    @IBOutlet weak var lemonInventoryLabel: UILabel!
+    @IBOutlet weak var iceInventoryLabel: UILabel!
+    @IBOutlet weak var lemonPurchasedLabel: UILabel!
+    @IBOutlet weak var icePurchasedLabel: UILabel!
+    @IBOutlet weak var lemonMixLabel: UILabel!
+    @IBOutlet weak var iceMixLabel: UILabel!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,52 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //IBActions for Buttons
+    
+    //Buy Lemon Button
+    @IBAction func lemonBuyButtonPressed(sender: AnyObject) {
+        println("Buy Lemon")
+    }
+    
+    //Refund Lemon Button
+    @IBAction func lemonRefundButtonPressed(sender: AnyObject) {
+        println("Refund Lemon")
+    }
+    
+    //Buy Ice Button
+    @IBAction func iceBuyButtonPressed(sender: AnyObject) {
+        println("Buy Ice")
+    }
+    
+    //Refund Ice Button
+    @IBAction func iceRefundButtonPressed(sender: AnyObject) {
+        println("Refund Ice")
+    }
+    
+    //Add Lemon to Mix Button
+    @IBAction func addLemonMixButtonPressed(sender: AnyObject) {
+        println("Lemon Added")
+    }
+    
+    //Remove Lemon from Mix Button
+    @IBAction func removeLemonMixButtonPressed(sender: AnyObject) {
+        println("Lemon Removed")
+    }
+    
+    //Add Ice to Mix button
+    @IBAction func addIceMixButtonPressed(sender: AnyObject) {
+        println("Add Ice")
+    }
 
+    //Remove Ice button Pressed
+    @IBAction func removeIceMixButtonPressed(sender: AnyObject) {
+        println("Remove Ice")
+    }
+
+    @IBAction func startButtonPressed(sender: AnyObject) {
+        println("Start Day")
+    }
 
 }
 
